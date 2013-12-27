1436325f83be56149ef5832f2a21c3b2e2d09cb2
benji <git@benjiweber.co.uk>
2013-12-27 18:58:09 +0000

    Fix Argument Tests

--- src/main/java/uk/co/benjiweber/benjibot/utils/Arguments.java
+++ src/main/java/uk/co/benjiweber/benjibot/utils/Arguments.java
@@ -1,5 +1,6 @@
 package uk.co.benjiweber.benjibot.utils;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -21,19 +22,26 @@ public class Arguments {
     private final List<String> args;
 
     public Arguments(PluginManager pluginManager, MessageEvent<BenjiBot> message) {
+        this(message.getMessage(), pluginManager, message);
+    }
+
+    private Arguments(String raw) {
+        this(raw, null, null);
+    }
+
+    private Arguments(String raw, PluginManager pluginManager, MessageEvent<BenjiBot> message) {
+        this.raw = raw;
+        String[] parts = raw.split(" ");
         this.pluginManager = pluginManager;
         this.message = message;
-        this.raw = message.getMessage();
-        String[] parts = raw.split(" ");
         Optional<String> pluginCommand = Optionals.flatten(ifHas(parts, 0).map(part -> validCommand(part) ? Optional.of(part) : Optional.<String>empty()));
         this.pluginName = pluginCommand.map(plugin -> plugin.substring(1).replaceAll("\\..*",""));
         this.commandName = pluginCommand.map(plugin -> plugin.substring(1).replaceAll(".*?\\.", ""));
         args = parts.length < 2
-            ? Lists.<String>newArrayList()
-            : Arrays.asList(parts)
+                ? Lists.<String>newArrayList()
+                : Arrays.asList(parts)
                 .subList(1, parts.length);
     }
-
     public Arguments(Arguments other, String raw, String[] parts) {
         this.raw = raw;
         this.pluginName = Optional.empty();
@@ -85,5 +93,9 @@ public class Arguments {
 
     public MessageEvent<BenjiBot> getMessage() {
         return message;
+    }
+
+    public static Arguments forTest(String raw) {
+        return new Arguments(raw);
     }
 }
--- src/test/java/uk/co/benjiweber/benjibot/utils/ArgumentsTest.java
+++ src/test/java/uk/co/benjiweber/benjibot/utils/ArgumentsTest.java
@@ -4,16 +4,50 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class ArgumentsTest {
 
-    @Test public void should_extract_pluginname_commandname_and_arguments() {
-       /* Arguments arguments = Arguments.fromString("tell.reply one two three");
+    Arguments arguments = Arguments.forTest("!tell.reply one two three four");
+
+    @Test public void should_extract_plugin_name() {
         Assert.assertEquals("tell", arguments.getPluginName().get());
-        Assert.assertEquals("reply", arguments.getCommandName().get());
-        Assert.assertEquals("one", arguments.getArg1().get());
-        Assert.assertEquals("two", arguments.getArg2().get());
-        Assert.assertEquals("three", arguments.getArg3().get());
-        Assert.assertEquals(Arrays.asList("one","two","three"), arguments.getArgs());                    */
     }
+
+    @Test public void should_extract_command_name() {
+        Assert.assertEquals("reply", arguments.getCommandName().get());
+    }
+
+    @Test public void should_not_extract_plugin_name_when_no_dot() {
+        Assert.assertEquals(Optional.<String>empty(), Arguments.forTest("!tellreply one two three four").getPluginName());
+    }
+
+    @Test public void should_not_extract_command_name_when_no_dot() {
+        Assert.assertEquals(Optional.<String>empty(), Arguments.forTest("!tellreply one two three four").getCommandName());
+    }
+
+    @Test public void should_not_extract_plugin_name_when_no_trigger() {
+        Assert.assertEquals(Optional.<String>empty(), Arguments.forTest("tell.reply one two three four").getPluginName());
+    }
+
+    @Test public void should_not_extract_command_name_when_no_trigger() {
+        Assert.assertEquals(Optional.<String>empty(), Arguments.forTest("tell.reply one two three four").getCommandName());
+    }
+
+    @Test public void should_extract_arg1() {
+        Assert.assertEquals("one", arguments.arg(1).get());
+    }
+
+    @Test public void should_extract_arg2() {
+        Assert.assertEquals("two", arguments.arg(2).get());
+    }
+
+    @Test public void should_extract_args_3plus() {
+        Assert.assertEquals("three four", arguments.argsFrom(3).get());
+    }
+
+    @Test public void should_extract_arg_list() {
+        Assert.assertEquals(Arrays.asList("one", "two", "three", "four"), arguments.getArgs());
+    }
+
 }
